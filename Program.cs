dcc83b0916eeed4147a70564b40aeb04e6bcbe47
Silas Henrique <silas.henrique21@outlook.com>
2024-01-10 13:17:46 -0300

    Atualizando classe de testes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "12345", modelo: "Modelo1", imei: "22222222", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Youtube");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone Iphone = new Iphone(numero: "987654", modelo: "Modelo5", imei: "66666666", memoria: 128);
+Iphone.ReceberLigacao();
+Iphone.InstalarAplicativo("Facebook");
+
